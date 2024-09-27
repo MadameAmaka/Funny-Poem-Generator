190339e60903f429ac864f48f4ed7485a05a13d3
Amaka Onwuli <AOnwuli@st-andrews.rochester.sch.uk>
2024-09-27 11:54:40 +0100

    Added the axios in html, basic styling, and added the API integration with API key, set up the context and the prompt

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generatePoem(event){
-    event.preventDefault();
-          new Typewriter("#poem", {
-        strings: "Roses are red, and so are my eyes",
+function displayPoem(response){
+         new Typewriter("#poem", {
+        strings: response.data.answer,
         autoStart: true,
         cursor: null,
-        delay: 10,
+        delay: 20,
       });
 }
+
+
+
+function generatePoem(event){
+    event.preventDefault();
+let instructionsInput=document.querySelector("#given-instructions");
+    let apiKey = "2f43379oac5f7ffe8tde5aff442f0cdb";
+    let prompt = `Generate a very short funny poem about ${instructionsInput.value}`;
+    let context = "You are a funny AI who loves to write short funny poems. Your task is to generate a 4-line short poem in basic html format and separate each line with a <br/>. Do not type the html out and please sign the poem with 'SheCodes AI' in <strong> and <small> element";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey} `
+
+    let poemDisplay = document.querySelector("#poem");
+ poemDisplay.innerHTML = "Generating your poem...";
+
+axios.get(apiUrl).then(displayPoem);
+}
+
+
+     
 
 
 
